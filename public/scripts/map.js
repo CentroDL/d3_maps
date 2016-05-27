c0d24302473b852703da4cc7c33412d028e4695f
Dennis Liaw <dennis.liaw@gmail.com>
2016-05-26 23:42:53 -0400

    adds queue object

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,3 +1,20 @@
-console.log("map.js loaded");
+  console.log("map.js loaded");
+
+
+var width = 960,
+    height = 600;
+
+var projection = d3.geo.albersUsa()
+    .scale(1280)
+    .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.select(self.frameElement).style("height", height + "px");
 
 
